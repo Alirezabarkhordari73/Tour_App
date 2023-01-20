141691df8c6616dff1f618c8b2a35fa0952f3d43
Alireza Barkhordari <aliirezaa.barkhordarii@gmail.com>
2023-01-20 21:47:07 +0330

    SErach-part

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const navbar = document.querySelector(".Header-Container .Navbar");
+const searchForm = document.querySelector(".Search-Form");
 
 document.querySelector("#Menu-Btn").onclick = () => {
   navbar.classList.add("active");
 };
 document.querySelector("#Nav-Close").onclick = () => {
   navbar.classList.remove("active");
+};
+document.querySelector("#Search-btn").onclick = () => {
+  searchForm.classList.add("active");
+};
+document.querySelector("#Close-Search").onclick = () => {
+  searchForm.classList.remove("active");
 };
